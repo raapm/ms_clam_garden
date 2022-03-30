1efad2d983ece68193d7207f7a2ec5523376d542
bensutherland <bensutherland7@gmail.com>
2022-03-30 11:02:48 -0700

    Update filtering and normalization code

--- 01_scripts/CG_MDS plots_G and DG separate.R
+++ 01_scripts/CG_MDS plots_G and DG separate.R
@@ -1,4 +1,4 @@
-# Data exploration script
+# Script to analyze transcriptomic data
 # Initialized to GitHub 2022-03-15
 
 # clear workspace
@@ -19,6 +19,188 @@ current.path <- dirname(rstudioapi::getSourceEditorContext()$path)
 current.path <- gsub(pattern = "\\/01_scripts", replacement = "", x = current.path) # take main directory
 setwd(current.path)
 
+options(scipen = 9999999)
+
+#### Method 1. cpm data ####
+# Set input filenames
+input.FN <- "02_input_data/out.matrix.csv"
+
+#### Read in raw count data ####
+# Import counts file (i.e. the final output from Simple_reads_to_counts repo)
+my.counts <- read.csv(file = "02_input_data/out.matrix.csv")
+my.counts[1:5,1:5]
+
+# Set first column as rownames
+rownames(my.counts) <- my.counts[,1]
+my.counts[1:5, 1:5]
+
+# Round all expression levels to no decimal place, remove transcript.id column (now rownames)
+my.counts.round <- round(my.counts[,-1])
+str(my.counts.round) # should be numeric
+my.counts.round[1:5, 1:5]
+
+# Prepare individual tissues datasets, and combined tissues (for MDS)
+# Create dataframe for all samples
+my.counts.round.all <- my.counts.round
+
+# Create dataframe for gill samples
+my.counts.round.gill <- my.counts.round[, grep(pattern = "\\.CG\\_G", x = colnames(my.counts.round))]
+dim(my.counts.round.gill)
+colnames(my.counts.round.gill)
+
+# Create dataframe for digestive gland (dig) samples
+my.counts.round.dig <- my.counts.round[, grep(pattern = "\\.CG\\_G", x = colnames(my.counts.round)
+                                              , invert = T)]
+dim(my.counts.round.dig)
+colnames(my.counts.round.dig)
+
+# Create vector for all three datatypes
+datatypes <- c("all", "gill", "dig")
+
+# Create list with all three datatypes
+input_dataframes.list <- list()
+input_dataframes.list[["all"]] <- my.counts.round.all
+input_dataframes.list[["gill"]] <- my.counts.round.gill
+input_dataframes.list[["dig"]] <- my.counts.round.dig
+
+
+#### Per datatype analysis, filter ####
+# User-set variables
+min.reads.mapping.per.transcript <- 10 # Variable to find an optimal cpm filt (edgeRuserguide suggests 5-10 reads mapping to transcript)
+min.ind <- 5 # choose the minimum number of individuals that need to pass the threshold
+
+# Use the list above and the three different datatypes to produce three different analyses, depending on the tissue type(s) included
+datatypes
+
+doi <- NULL; doi.summary <- list(); cpm.filt <- NULL; keep <- NULL; doi.DGEList.filt <- list()
+for(i in 1:length(datatypes)){
+  
+  # Select the datatypes
+  doi <- input_dataframes.list[[datatypes[i]]]
+  
+  # create DGElist
+  doi.DGEList <- DGEList(counts = doi)
+  
+  # TODO: ADD PHENOTYPES HERE to the DGEList
+  
+  # Statistics on library sizes
+  doi.summary[[paste0(datatypes[[i]], "_align_summary")]]    <- summary(doi.DGEList$samples$lib.size)
+  doi.summary[[paste0(datatypes[[i]], "_align_summary_sd")]] <- sd(doi.DGEList$samples$lib.size)
+  
+  # Determine cpm.filt for the library with the fewest transcripts to be the most stringent on low expression cutoff
+  cpm.filt <- min.reads.mapping.per.transcript / min(doi.DGEList$samples$lib.size) * 1000000
+  print(paste0("The cpm filter threshold for ", datatypes[i], " is determined as: ", cpm.filt)) # min cpm filt
+  doi.summary[[paste0(datatypes[[i]], "_cpm.filt")]] <- cpm.filt
+  
+  # Identify tags passing filter
+  keep <- rowSums(cpm(doi.DGEList)>cpm.filt) >= min.ind # Find which transcripts pass the filter
+  table(keep) # gives number passing, number failing
+  doi.summary[[paste0(datatypes[[i]], "_post-filter_retained")]] <- table(keep)
+  
+  # Subset DGEList to only those passing expression filter
+  doi.DGEList <- doi.DGEList[keep, , keep.lib.sizes=FALSE] #keep.lib.sizes = T retains original lib sizes, otherwise recomputes w remaining tags
+  dim(doi.DGEList)
+  
+  # Save out the filtered DGEList
+  doi.DGEList.filt[[datatypes[i]]] <- doi.DGEList
+  
+  print("Your data is saved as a slot in doi.DGEList.filt")
+  print("Summaries of raw data and filtering steps are in doi.summary")
+  
+}
+
+
+
+# # Select which one you want to use, then run the rest of the 
+# my.counts.round <- my.counts.round;       datatype <- "all" # all
+# #my.counts.round <- my.counts.round.gill;  datatype <- "gill" # Gill only
+# #my.counts.round <- my.counts.round.dig;   datatype <- "dig" # Digestive gland
+
+
+#### 3. Normalization and Data Visualization ####
+# Use the list above (doi.DGEList.filt) and the three different datatypes to produce three different analyses, depending on the tissue type(s) included
+datatypes
+
+doi <- NULL
+for(i in 1:length(datatypes)){
+  
+  # Select the datatypes
+  doi <- doi.DGEList.filt[[datatypes[i]]]
+  
+  # Use TMM normalization, as it takes into account highly expressed genes that may take up sequencing rxn and make other genes look down-reg.
+  doi <- calcNormFactors(doi, method = c("TMM"))
+  doi$samples
+  
+  # Statistics on library sizes
+  doi.summary[[paste0(datatypes[[i]], "_norm_factors")]]    <- doi$samples
+  
+  # Plot normalization factors
+  pdf(file = paste0("04_txomic_results/", datatypes[i], "_norm_factors.pdf"), width = 6, height = 6)
+  plot(doi$samples$norm.factors ~ doi$samples$lib.size
+       , xlab = "Library Size", ylab = paste0("Normalization factors (", datatypes[i], ")")
+       , las = 1)
+  dev.off()
+  
+  # Estimate dispersions (measure inter-library variation per tag)
+  doi <- estimateDisp(doi) # note that this can use a design matrix when provided 
+  doi$prior.df # est. overall var. across genome for dataset
+  doi.summary[[paste0(datatypes[[i]], "_overall_var")]] <- doi$prior.df
+  
+  print("The coefficient of variation for biological variation for this dataset is: ")
+  sqrt(doi$common.disp) #coeff of var, for biol. var
+  doi.summary[[paste0(datatypes[[i]], "_coeff_var_biol")]] <- sqrt(doi$common.disp)
+  
+  # Plot Biological coefficient of variation by mean log cpm
+  pdf(file = paste0("04_txomic_results/", datatypes[i], "_BCV_by_mean_log_cpm.pdf"), width = 6, height = 6)
+  plotBCV(doi, ylab = paste0("Biological coefficient of variation (", datatypes[i] ,")"))
+  dev.off()
+  
+  # MDS plot
+  pdf(file = paste0("04_txomic_results/", datatypes[i], "_mds_dim_1_2.pdf"), width = 9, height = 5)
+  plotMDS(x = doi, cex= 0.8) # note that this is supposed to be run on whatever you wrote calcNormFact() to
+  dev.off()
+  
+  # MDS plotting options to work with:
+  # labels = character vector of sample names or labels. Defaults to colnames(x).
+  # pch = plotting symbol or symbols. See points for possible values. Ignored if labels is non-NULL.
+  # TODO: this will be adjusted once the phenotypes are brought in
+  
+  }
+
+
+
+
+
+
+###### If need to export #####
+#### 4. Prepare Output ####
+# # generate CPM matrix
+# normalized.output <- cpm(my.counts, normalized.lib.sizes = TRUE, log= F)
+# 
+# # Compare the raw counts to the normalized cpm values (not log)
+# my.counts$counts[1:5, 1:5] # not normalized, raw counts
+# normalized.output[1:5, 1:5] # normalized lib size calculated cpm values
+# 
+# # output as normalized linear
+# #write.csv(normalized.output, file = "03_normalized_data/normalized_output_matrix.csv")
+# #write.csv(normalized.output, file = "03_normalized_data/normalized_output_matrix_gill.csv")
+# #write.csv(normalized.output, file = "03_normalized_data/normalized_output_matrix_dig.csv")
+# 
+# # # output as normalized log2 (in progress)
+# normalized.output.log2 <- cpm(my.counts, normalized.lib.sizes = TRUE, log= T, prior.count = 1)
+# #write.csv(normalized.output, file = "03_normalized_data/normalized_output_matrix_log2.csv")
+# #write.csv(normalized.output, file = "03_normalized_data/normalized_output_matrix_log2_gill.csv")
+# #write.csv(normalized.output, file = "03_normalized_data/normalized_output_matrix_log2_dig.csv")
+# 
+# # output object
+# save.image(file = "02_input_data/sfon_wgcna_01_output.RData") # save out existing data 
+# 
+
+#### END NEW ####
+
+
+#### Method 2. Original Files ####
+# Goes from input data through normalization to MDS plot
 # Set input filenames
 input.FN <- "02_input_data/cgrnaseqBTv1.csv" # this is the input filename for "CG_MDS plots_G and DG separate.R"
 annot.FN <- "02_input_data/project155.uniprot_blastp.txt.gz"
